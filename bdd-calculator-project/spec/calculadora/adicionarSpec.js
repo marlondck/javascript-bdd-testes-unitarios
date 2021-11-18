211727d3d504096ced35a5e114a026e8921e6255
marlondck <15050029+marlondck@users.noreply.github.com>
2021-11-18 16:05:03 -0400

    ajuste diretorio src

--- bdd-calculator-project/spec/calculadora/adicionarSpec.js
+++ bdd-calculator-project/spec/calculadora/adicionarSpec.js
@@ -1,5 +1,5 @@
 describe("Suite de testes de adição", () => {
-  const calculadora = require("../src/js/calculadora");
+  const calculadora = require("../../src/js/calculadora");
 
   it("deve retornar 5 da soma de 2 e 3", () => {
     expect(calculadora.adicionar(2, 3)).toEqual(5);
