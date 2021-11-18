fa6875afcc66760c3a26985d92c5c025b7d52669
marlondck <15050029+marlondck@users.noreply.github.com>
2021-11-18 15:59:45 -0400

    func: adição test: true, ok

--- bdd-calculator-project/spec/calculadora/adicionarSpec.js
+++ bdd-calculator-project/spec/calculadora/adicionarSpec.js
@@ -4,4 +4,16 @@ describe("Suite de testes de adição", () => {
   it("deve retornar 5 da soma de 2 e 3", () => {
     expect(calculadora.adicionar(2, 3)).toEqual(5);
   });
+
+  it("deve retornar 6 para a soma de 9 e -3 no formato texto", () => {
+    expect(calculadora.adicionar("9", "-3")).toEqual(6);
+  });
+
+  it("deve retornar 4.5 para soma de 1.5 e 3", () => {
+    expect(calculadora.adicionar(1.5, 3)).toEqual(4.5);
+  });
+
+  it("deve retornar 0 quando valor 1 não for numérico", () => {
+    expect(calculadora.adicionar(undefined, 10)).toEqual(0);
+  });
 });
--- bdd-calculator-project/spec/src/js/calculadora.js
+++ bdd-calculator-project/spec/src/js/calculadora.js
@@ -1,5 +1,10 @@
 const calculadora = {
-  adicionar: (n1, n2) => 0,
+  adicionar: (n1, n2) => {
+    if (isNaN(n1) || isNaN(n2)) {
+      return 0;
+    }
+    return Number(n1) + Number(n2);
+  },
   subtrair: (n1, n2) => 0,
   dividir: (n1, n2) => 0,
   multiplicar: (n1, n2) => 0,
